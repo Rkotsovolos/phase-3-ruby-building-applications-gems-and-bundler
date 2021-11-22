8b1886bdd8402561992d59987e320b114027fbb9
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-11-22 14:56:36 -0800

    commit tree 0b3719d3c74989e056c6d05041c8ef704974ba87

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,6 @@
 # require the environment file
+# require_relative '../config/enviroment'
+# require "bundler/setup"
+# Bundler.require(:default, :development) 
+# hash = { :a => 4, :b => 10 }
+# ap hash
